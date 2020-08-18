77b08aba25e1a38e18b169b5a80ff81a9505ae50
Gage Thornberry <gagedoug13@gmail.com>
2020-08-18 13:06:52 -0600

    adding discovery portion in order to implement web hooks

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('hi')
+const tmi = require('tmi.js')
+const fs = require('fs')
+
+require('dotenv').config()
+
+const opts = {
+    identity: {
+        username: process.env.BOT_CHANNEL,
+        password: process.env.PASS,
+    }, 
+    channels: [
+        process.env.MY_CHANNEL
+    ]
+}
+
+const client = new tmi.client(opts)
