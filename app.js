777c9aadee1534246d3e0df9e37a564493bba78d
Gage Thornberry <gagedoug13@gmail.com>
2020-08-18 18:18:02 -0600

    successfully capturing users data and combining it with a time stamp

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const tmi = require('tmi.js')
 const fs = require('fs')
+const request = require('request')
 
-require('dotenv').config()
+let collection = []
+request('https://tmi.twitch.tv/group/user/nylume/chatters', function (error, response, body) {
 
-const opts = {
-    identity: {
-        username: process.env.BOT_CHANNEL,
-        password: process.env.PASS,
-    }, 
-    channels: [
-        process.env.MY_CHANNEL
-    ]
-}
+    const timeStamp = new Date();
+    
+    const timeStampAndViewers = [{"timeStamp": timeStamp}]
+    
+    timeStampAndViewers.push({'allChatters': body})
+    
+    collection.push(timeStampAndViewers)
+    
+    console.log(collection)
 
-const client = new tmi.client(opts)
+
+
+})
+// require('dotenv').config()
+// fetch("tmi.twitch.tv/groups/user/nylume/chatters") 
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+    
+
+// const client = new tmi.client(opts)
--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const cors = require('cors')
-
-
-
- 
-app = express()
-
