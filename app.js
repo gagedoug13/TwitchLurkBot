a9ca8f1699010f9ecb57c9b2bd7e7152b7a70c05
Gage Thornberry <gagedoug13@gmail.com>
2020-08-19 21:53:49 -0600

    connected to the mongo  cluster. working on sending data to the cluster

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const tmi = require('tmi.js')
 const fs = require('fs')
 const request = require('request')
 
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://gagedoug13:Biscuit62!@cluster0.icdxr.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+
 let collection = []
 request('https://tmi.twitch.tv/group/user/nylume/chatters', function (error, response, body) {
 
