9c413d10fb29d1dfb1453b50c804982192ae0bcc
Gage Thornberry <gagedoug13@gmail.com>
2020-08-20 22:58:33 -0600

    cleaned  up code

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 const request = require('request')
-
 require('dotenv').config()
+
+const { MongoClient } = require('mongodb')
+
+const uri = process.env.DB_URL
+const client = new MongoClient(uri)
 
 let db = null
 let userCollection = null
 
 
-const { MongoClient } = require('mongodb')
-const uri = process.env.DB_URL
-
-const client = new MongoClient(uri)
-
 client.connect().then(() => {
   userCollection = client.db("lurkBase").collection("lurkData");
-  // perform actions on the collection 
- 
   console.log("connected")
 })
-
 
 
 const addRecordToCollection = setInterval(() => {
@@ -28,8 +24,7 @@ const addRecordToCollection = setInterval(() => {
     const timeStampAndViewers = [{"timeStamp": timeStamp}]
     
     timeStampAndViewers.push({'allChatters': body})
-    
-  //  collection.push(timeStampAndViewers)
+
     userCollection.insertOne({timeStampAndViewers}) 
 
 })}, 60000)
