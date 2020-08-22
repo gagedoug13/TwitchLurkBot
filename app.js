16d55a2ee1d02fc8436e310bc089914c3fde03f1
Gage Thornberry <gagedoug13@gmail.com>
2020-08-22 01:53:56 -0600

    created sandbox to visual data and model it. also calling lookForUser on interval

--- app.js
+++ app.js
@@ -15,21 +15,39 @@ client.connect().then(() => {
   console.log("connected")
 })
 
+const lookForUser = async () => {
+  const userRecord = await userCollection.findOne({userName: "nation_live"}, {}, function(err, doc) {
+    return doc
+  })
+  if (userRecord) {
+    console.log("nation found") 
+  } else {
+    console.log("no user found")
+  }
+}
+
 
 const addRecordToCollection = setInterval(() => {
-  request('https://tmi.twitch.tv/group/user/nylume/chatters', function (error, response, body) {
+  request({url: 'https://tmi.twitch.tv/group/user/nylume/chatters', json: true}, function (error, response, body) {
 
+    lookForUser()
+  
     const timeStamp = new Date();
     
     const timeStampAndViewers = [{"timeStamp": timeStamp}]
     
     timeStampAndViewers.push({'allChatters': body})
+    const streamer = timeStampAndViewers[1].allChatters.chatters.broadcaster[0]
+    // console.log(timeStampAndViewers)
+    // userCollection.insertOne({timeStampAndViewers}, function(err, r) {
+     
+    // });
+    // console.log(timeStampAndViewers)
 
-    userCollection.insertOne({timeStampAndViewers}) 
+    // userCollection.insertOne({timeStampAndViewers}) 
 
-})}, 60000)
+    // userCollection.drop(function(err, reply) {
+    //   // deletes userCollection database
+    // })
 
-
-
-
-
+})}, 1000)
