a19686cd44ac4a7d172698d71172243e334ff437
Gage Thornberry <gagedoug13@gmail.com>
2020-08-23 17:53:18 -0600

    Contructing the application to functionally handle writing to DB when necessary

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const { MongoClient } = require('mongodb')
 const uri = process.env.DB_URL
 const client = new MongoClient(uri)
 
-let db = null
 let userCollection = null
+let currentStream = null
+const streamer = null
 
 
 client.connect().then(() => {
@@ -15,39 +16,48 @@ client.connect().then(() => {
   console.log("connected")
 })
 
-const lookForUser = async () => {
-  const userRecord = await userCollection.findOne({userName: "nation_live"}, {}, function(err, doc) {
-    return doc
-  })
-  if (userRecord) {
-    console.log("nation found") 
-  } else {
-    console.log("no user found")
-  }
+const findCurrentStream = async(streamer) => {
+
+  // if (!currentStream) {
+  //    create a new stream object in the user's streams. 
+  //    currentStream = true
+  //    RETURN THE STREAM OBJECT FOR NEXT FUNCTION
+  // } else {
+  //    FIND AND RETURN THE STREAM OBJECT
+  // }
 }
 
 
-const addRecordToCollection = setInterval(() => {
+const addScreenShot = (streamObject) => {
+//  ADD THE SCREENSHOT TO THE USER'S CURRENTSTREAM OBJECT
+}
+
+
+const addRecordToCollection = setInterval((streamer) => {
   request({url: 'https://tmi.twitch.tv/group/user/nylume/chatters', json: true}, function (error, response, body) {
 
-    lookForUser()
-  
+    streamer = timeStampAndViewers[1].allChatters.chatters.broadcaster[0]
+
+    findCurrentStream(streamer)
+
     const timeStamp = new Date();
     
     const timeStampAndViewers = [{"timeStamp": timeStamp}]
     
     timeStampAndViewers.push({'allChatters': body})
-    const streamer = timeStampAndViewers[1].allChatters.chatters.broadcaster[0]
-    // console.log(timeStampAndViewers)
-    // userCollection.insertOne({timeStampAndViewers}, function(err, r) {
-     
-    // });
-    // console.log(timeStampAndViewers)
-
-    // userCollection.insertOne({timeStampAndViewers}) 
-
-    // userCollection.drop(function(err, reply) {
-    //   // deletes userCollection database
-    // })
 
 })}, 1000)
+
+
+
+
+
+
+// MONGO METHODS  -
+
+// const user = await userCollection.find({userName: "nation_live"}).limit(1)
+
+
+// userCollection.drop(function(err, reply) {
+//    deletes userCollection database
+// })
